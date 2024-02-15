a64404fa4d289a3c66a313d970e828bc582a7b48
jrshittu <misterjrshittu@gmail.com>
2024-02-15 18:55:15 +0100

    add required docs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Route, Routes, BrowserRouter } from "react-router-dom"
+import { Box } from '@mui/material'
+import './App.css'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import ExerciseDetail from './pages/ExerciseDetail'
 
 const App = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Box width="400px">
+      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercisse/:id" element={<ExerciseDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </Box>
   )
 }
 
